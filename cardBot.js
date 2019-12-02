d6f2c995ad2c1344694eae64593d29bdd345fd07
Santiago Pravisani <santiagopravisani@gmail.com>
2019-12-02 17:36:03 -0300

    Updated for tutorial

--- cardBot.js
+++ cardBot.js
@@ -4,19 +4,6 @@ const bot = new WebexChatBot();
 const SparkAPIWrapper = require("node-sparkclient");
 const client = new SparkAPIWrapper(process.env.ACCESS_TOKEN);
 
-//
-// Bots commands here
-//
-bot.onCommand("hello", function (command) {
-  let email = command.message.personEmail; // User that created the message orginally
-  client.createMessage(command.message.roomId, `Hello, your email is: **${email}**`, { "markdown":true }, function(err, message) {
-      if (err) {
-          console.log("WARNING: could not post message to room: " + command.message.roomId);
-          return;
-      }
-  });
-});
-
 attachment = {
   contentType: 'application/vnd.microsoft.card.adaptive',
   content: {
